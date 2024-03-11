5fb028ee247aff3e331b6de8b3df395504666229
Aarron Hulswitt <aarron.hulswitt@gmail.com>
2024-03-11 08:54:10 -0400

    added templates for nvim.

---
+++ lua/kameleon/lazy/template.lua
@@ -0,0 +1,14 @@
+return
+{
+    'glepnir/template.nvim',
+    cmd = {'Template','TemProject'},
+    config = function()
+        require('template').setup({
+            temp_dir = '~/.config/nvim/lua/kameleon/lazy/templates',
+        })
+
+        vim.keymap.set('n', '<leader>nc', function()
+            vim.fn.feedkeys(":Template ")
+        end, {remap = true})
+    end
+}
---
+++ lua/kameleon/lazy/templates/class.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace {{_lua:vim.fn.expand('%:p:h')_}};
+
+public class {{_file_name_}}
+{
+    {{_cursor_}}
+}
