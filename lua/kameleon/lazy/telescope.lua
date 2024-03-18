227db5cf9c7e06110a6c94dde836173e6714e1cc
Aarron Hulswitt <ahulswitt@embracepetinsurance.com>
2024-03-18 12:41:57 -0400

    Added RipGrep so that Telescope live Grep works

--- lua/kameleon/lazy/telescope.lua
+++ lua/kameleon/lazy/telescope.lua
@@ -5,7 +5,8 @@ return
         tag = "0.1.5",
 
         dependencies = {
-            "nvim-lua/plenary.nvim"
+            "nvim-lua/plenary.nvim",
+            "BurntSushi/ripgrep"
         },
 
 
@@ -15,6 +16,7 @@ return
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
             vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+            vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>pws', function()
                 local word = vim.fn.expand("<cword>")
                 builtin.grep_string({ search = word })
@@ -23,9 +25,9 @@ return
                 local word = vim.fn.expand("<cWORD>")
                 builtin.grep_string({ search = word })
             end)
-            vim.keymap.set('n', '<leader>ps', function()
-                builtin.grep_string({ search = vim.fn.input("Grep > ") })
-            end)
+           -- vim.keymap.set('n', '<leader>ps', function()
+           --     builtin.grep_string({ search = vim.fn.input("Grep > ") })
+           -- end)
             vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
         end
     }
