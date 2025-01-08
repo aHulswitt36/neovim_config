ae6ba197d74001c7f7c8ea88906a1066ea13b3b1
Aarron Hulswitt <aarron.hulswitt@gmail.com>
2025-01-08 07:03:02 -0500

    Adding the Roslyn and RZLS LSPs to be able to work with Blazor files.  Removed Omnisharp from Mason, as we will use Roslyn for C#

---
+++ lua/kameleon/lazy/roslyn.lua
@@ -0,0 +1,75 @@
+return {
+    {
+        -- 'jmederosalvarado/roslyn.nvim',
+        'seblj/roslyn.nvim',
+        ft = { 'cs', 'razor' },
+        dependencies = {
+            {
+                'tris203/rzls.nvim',
+            },
+        },
+        init = function()
+            vim.filetype.add {
+                extension = {
+                    razor = 'razor',
+                    cshtml = 'razor',
+                },
+            }
+        end,
+        config = function()
+
+            local cmp_lsp = require("cmp_nvim_lsp")
+            local capabilities = vim.tbl_deep_extend(
+                "force",
+                {},
+                vim.lsp.protocol.make_client_capabilities(),
+                cmp_lsp.default_capabilities())
+
+            require('rzls').setup {
+                capabilities = capabilities,
+            }
+            require('roslyn').setup {
+                args = {
+                    '--logLevel=Information',
+                    '--extensionLogDirectory=' .. vim.fs.dirname(vim.lsp.get_log_path()),
+                    '--razorSourceGenerator='
+                        .. vim.fs.joinpath(vim.fn.stdpath 'data' --[[@as string]], 'mason', 'packages', 'roslyn', 'libexec', 'Microsoft.CodeAnalysis.Razor.Compiler.dll'),
+                    '--razorDesignTimePath=' .. vim.fs.joinpath(
+                        vim.fn.stdpath 'data' --[[@as string]],
+                        'mason',
+                        'packages',
+                        'rzls',
+                        'libexec',
+                        'Targets',
+                        'Microsoft.NET.Sdk.Razor.DesignTime.targets'
+                    ),
+                },
+                config = {
+                    on_attach = function()end,--require 'lspattach',
+                    capabilities = capabilities, --require 'lspcapabilities',
+                    handlers = require 'rzls.roslyn_handlers',
+                    settings = {
+                        ['csharp|inlay_hints'] = {
+                            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+                            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+
+                            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+                            csharp_enable_inlay_hints_for_types = true,
+                            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+                            dotnet_enable_inlay_hints_for_literal_parameters = true,
+                            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+                            dotnet_enable_inlay_hints_for_other_parameters = true,
+                            dotnet_enable_inlay_hints_for_parameters = true,
+                            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+                            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+                            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+                        },
+                        ['csharp|code_lens'] = {
+                            dotnet_enable_references_code_lens = true,
+                        },
+                    },
+                },
+            }
+        end,
+    },
+}
