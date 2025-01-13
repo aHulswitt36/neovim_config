26a4f2196f8bdd7dd178ba86adcc3aa20889e4fa
Aarron Hulswitt <aarron.hulswitt@gmail.com>
2025-01-12 22:46:16 -0500

    updates to Autopairs and LSP.  Adding in Actions-Preview, Editing, and Git

---
+++ lua/kameleon/lazy/git.lua
@@ -0,0 +1,10 @@
+return
+    {
+        'lewis6991/gitsigns.nvim',
+        -- event = "VeryLazy",
+        cmd = "Gitsigns",
+        opts = {},
+        -- config = function()
+        -- 	require('gitsigns').setup()
+        -- end
+    }
--- lua/kameleon/lazy/lsp.lua
+++ lua/kameleon/lazy/lsp.lua
@@ -12,10 +12,12 @@ return
             "L3MON4D3/LuaSnip",
             "saadparwaiz1/cmp_luasnip",
             "j-hui/fidget.nvim",
+			'hrsh7th/cmp-nvim-lsp-signature-help',
         },
 
         config = function()
             local cmp = require("cmp")
+            require("luasnip.loaders.from_vscode").lazy_load();
             local cmp_lsp = require("cmp_nvim_lsp")
             local capabilities = vim.tbl_deep_extend(
                 "force",
@@ -66,15 +68,27 @@ return
                         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
                     end,
                 },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
                 mapping = cmp.mapping.preset.insert({
-                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                 }),
+                -- mapping = cmp.mapping.preset.insert({
+                --     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                --     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                --     ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                --     ["<C-Space>"] = cmp.mapping.complete(),
+                -- }),
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
                     { name = 'luasnip' }, -- For luasnip users.
+                    { name = 'nvim_lsp_signature_help' }
                 }, {
                         { name = 'buffer' },
                     })
