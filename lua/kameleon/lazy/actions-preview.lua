972ab676a396c3456956632109702ace8d228702
aHulswitt36 <aarron.hulswitt@gmail.com>
2025-01-16 07:13:59 -0500

    update to actions-preview

--- lua/kameleon/lazy/actions-preview.lua
+++ lua/kameleon/lazy/actions-preview.lua
@@ -4,5 +4,28 @@ return {
     -- lazy = false,
     config = function()
         vim.keymap.set({ "v", "n" }, "<leader>la", require("actions-preview").code_actions)
-    end,
+
+        require("actions-preview").setup {
+            -- nui = {
+            --     layout = {
+            --         size = {
+            --             width = "80%"
+            --         }
+            --     }
+            -- }
+            telescope = {
+                sorting_strategy = "ascending",
+                layout_strategy = "vertical",
+                layout_config = {
+                    width = 0.8,
+                    height = 0.9,
+                    prompt_position = "top",
+                    preview_cutoff = 20,
+                    preview_height = function(_, _, max_lines)
+                        return max_lines - 15
+                    end,
+                },
+            },
+        }
+    end, 
 }
